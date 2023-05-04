05ab43edfcfcb339f21c71d712e9d28d3e494522
hudaifa123 <hodaeyfha@gmail.com>
2023-05-05 02:37:12 +0300

    Using Celery with Django

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,27 +22,37 @@ DATA_DIR = BASE_DIR / "data"
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5kn_abaosy8!k78=+ijnsimthypn_53xkx1vfzmaeq)cr1s&73"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", default=0, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
+    "django_celery_beat",
+    "django_celery_results",
+]
+
+LOCAL_APPS = [
     "movies",
     "core",
     "ratings",
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -54,6 +65,36 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "config.urls"
+
+# celery
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "django-db"
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
+# CELERY_RESULT_EXTENDED = True
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-backend-always-retry
+# # https://github.com/celery/celery/pull/6122
+# CELERY_RESULT_BACKEND_ALWAYS_RETRY = True
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-backend-max-retries
+# CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-accept_content
+# CELERY_ACCEPT_CONTENT = ["json"]
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-task_serializer
+# CELERY_TASK_SERIALIZER = "json"
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_serializer
+# CELERY_RESULT_SERIALIZER = "json"
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-time-limit
+# # TODO: set to whatever value is adequate in your circumstances
+# CELERY_TASK_TIME_LIMIT = 5 * 60
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-soft-time-limit
+# # TODO: set to whatever value is adequate in your circumstances
+# CELERY_TASK_SOFT_TIME_LIMIT = 60
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
+# CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-send-task-events
+# CELERY_WORKER_SEND_TASK_EVENTS = True
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
+# CELERY_TASK_SEND_SENT_EVENT = True
 
 TEMPLATES = [
     {
