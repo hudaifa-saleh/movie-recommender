f30ae94a1edcb0287343f3f6947d420cf4fc17f6
hudaifa123 <hodaeyfha@gmail.com>
2023-05-05 03:10:22 +0300

    id

--- ratings/admin.py
+++ ratings/admin.py
@@ -4,7 +4,7 @@ from .models import Rating
 
 
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ["content_object", "user", "value", "active"]
+    list_display = ["content_object", "id", "user", "value", "active"]
     search_fields = ["user__username"]
     raw_id_fields = ["user"]
     readonly_fields = ["content_object"]
